4bcf336b8bc13b15447af138d6759ac12713b0b1
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-06-01 17:16:56 +0900

    fix:typeをinterface二変更

--- household_account/types/accountData.ts
+++ household_account/types/accountData.ts
@@ -1,6 +1,6 @@
 export type IncomeOrExpenditure = "INCOME" | "EXPENDITURE";
 
-export type AccountData = {
+export interface AccountData  {
     id: number;
     name: string;
     category:string;
@@ -8,7 +8,7 @@ export type AccountData = {
     balanceOfPayment: string;
 };
 
-export type CategoryData = {
+export interface CategoryData  {
     category: string;
     categoryId: number;
     price: number;
